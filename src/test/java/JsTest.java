4a15fa71d35a4db4093db68c4c676320f2b64baa
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2021-03-08 13:02:40 +0300

    split test, add docstrings for each test and class

--- src/test/java/JsTest.java
+++ src/test/java/JsTest.java
@@ -3,16 +3,34 @@ import org.junit.Test;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.url;
 
+/**
+ * test fail on HtmlUnit with JS enabled
+ * test pass on HtmlUnit with JS disabled
+ * default for HtmlUnit: javaScript is off
+ * test pass on Chrome
+ */
 public class JsTest extends AbstractWebTest {
 
-    /**
-     * to make test pass comment switchOnJavaScript
-     * to make test fail uncomment switchOnJavaScript
-     * default for htmlUnit: javaScript is off
-     */
+    /** Test fail on HtmlUnit with JS enabled */
     @Test
-    public void testHtmlUnitWithJS() throws Exception {
-        //switchOnJavaScript();
+    public void testOnHtmlUnitWithJS() throws Exception {
+        switchOnJavaScript();
+        open("http://www.pearl.de/authorization/login.do");
+        System.out.println("url opened: " + url());
+        System.out.println("testHtmlUnitWithJS: passed");
+    }
+
+    /** Test pass on HtmlUnit with JS disabled */
+    @Test
+    public void testOnHtmlUnitWithoutJS() throws Exception {
+        open("http://www.pearl.de/authorization/login.do");
+        System.out.println("url opened: " + url());
+        System.out.println("testHtmlUnitWithJS: passed");
+    }
+    
+    /** Test pass on Chrome */
+    @Test
+    public void testOnChrome() throws Exception {
         open("http://www.pearl.de/authorization/login.do");
         System.out.println("url opened: " + url());
         System.out.println("testHtmlUnitWithJS: passed");
